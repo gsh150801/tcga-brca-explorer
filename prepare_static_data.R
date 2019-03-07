9425e8f09938fac0f50324f47deb5cb409774c8e
John Lövrot <john.lovrot@ki.se>
2019-03-07 17:48:55 +0100

    styler::style_dir()

--- prepare_static_data.R
+++ prepare_static_data.R
@@ -4,16 +4,20 @@ require(org.Hs.eg.db)
 data("pam50", package = "genefu")
 pam50centroids <- pam50$centroids
 
-rownames(pam50centroids) <- unlist(mget(
-  as.character(pam50$centroids.map$EntrezGene.ID), org.Hs.egSYMBOL, 
-  ifnotfound = NA))
+rownames(pam50centroids) <- unlist(
+  mget(
+    as.character(pam50$centroids.map$EntrezGene.ID), org.Hs.egSYMBOL,
+    ifnotfound = NA
+  )
+)
 
 lkup <- c(
-  "LumA" = "LA", 
-  "LumB" = "LB", 
-  "Her2" = "H2", 
-  "Basal" = "BL", 
-  "Normal" = "NBL")
+  "LumA" = "LA",
+  "LumB" = "LB",
+  "Her2" = "H2",
+  "Basal" = "BL",
+  "Normal" = "NBL"
+)
 colnames(pam50centroids) <- lkup[colnames(pam50$centroids)]
 
 save(pam50centroids, file = file.path("data", "pam50centroids.rda"))
--- server.R
+++ server.R
@@ -8,8 +8,8 @@ load(file.path("data", "pam50centroids.rda"))
 source("utility_functions.R")
 
 ggplot2::theme_set(theme_classic() +
-    theme(axis.line.x = element_blank()) + 
-    theme(axis.line.y = element_blank()))
+  theme(axis.line.x = element_blank()) +
+  theme(axis.line.y = element_blank()))
 
 colmutcat <- c("(germline)" = "black", "mutated" = "#1070b8")
 alphamutcat <- c("(germline)" = 0.5, "mutated" = 1)
@@ -19,35 +19,39 @@ conn <- CGDS("http://www.cbioportal.org/")
 subtype_data <- perform_subtype_classification(conn, pam50centroids)
 
 function(input, output) {
-  
   conn <- CGDS("http://www.cbioportal.org/public-portal/")
-  
+
   retrieved_tcga_data <- reactive({
     input$retrieve_data_button
     ids <- split_query_str(isolate(input$query_str))
     retrieve_tcga_data(conn, ids)
   })
-  
+
   output$retrieved_genes <- renderUI({
-    p("Data retrieved for genes:",
+    p(
+      "Data retrieved for genes:",
       lapply(retrieved_tcga_data()$ids, function(x)
         a(x,
           href = paste0("http://www.genecards.org/cgi-bin/carddisp.pl?gene=", x),
-          target = "_blank")))
+          target = "_blank"
+        ))
+    )
   })
 
-  output$var_y_ui = renderUI({
+  output$var_y_ui <- renderUI({
     ids <- retrieved_tcga_data()$ids
-    selectInput("var_y", "Gene on vertical axis", 
-      choices = ids, selected = ids[1])
+    selectInput("var_y", "Gene on vertical axis",
+      choices = ids, selected = ids[1]
+    )
   })
-  
-  output$var_x_ui = renderUI({
+
+  output$var_x_ui <- renderUI({
     ids <- retrieved_tcga_data()$ids
-    selectInput("var_x", "Gene on horizontal axes", 
-      choices = ids, selected = ids[min(2, length(ids))])
+    selectInput("var_x", "Gene on horizontal axes",
+      choices = ids, selected = ids[min(2, length(ids))]
+    )
   })
-  
+
   assembled_graphics_data <- reactive({
     ids <- retrieved_tcga_data()$ids
     var_x <- input$var_x
@@ -55,40 +59,43 @@ function(input, output) {
     if (is.null(var_x) | is.null(var_y)) {
       var_x <- ids[min(2, length(ids))]
       var_y <- ids[1]
-    } 
+    }
     if (!(var_x %in% ids)) {
       var_x <- ids[min(2, length(ids))]
     }
     if (!(var_y %in% ids)) {
       var_y <- ids[1]
     }
-    
+
     graphics_data <- retrieved_tcga_data()$data %>%
       mutate_(
-        x_mut = paste0(var_x, "_mutations"), 
-        x_gistic = paste0(var_x, "_gistic"), 
-        x_rna = paste0(var_x, "_rna"), 
-        y = paste0(var_y, "_rna")) %>%
+        x_mut = paste0(var_x, "_mutations"),
+        x_gistic = paste0(var_x, "_gistic"),
+        x_rna = paste0(var_x, "_rna"),
+        y = paste0(var_y, "_rna")
+      ) %>%
       mutate(
-        x_mutcat = 
+        x_mutcat =
           factor(x_mut == "(germline)",
             levels = c(TRUE, FALSE),
-            labels = c("(germline)", "mutated"))) %>%
-      '['(c("subjid", "x_mut", "x_mutcat", "x_gistic", "x_rna", "y")) %>%
+            labels = c("(germline)", "mutated")
+          )
+      ) %>%
+      "["(c("subjid", "x_mut", "x_mutcat", "x_gistic", "x_rna", "y")) %>%
       left_join(subtype_data, by = "subjid")
     graphics_data
   })
-  
+
   output$tab1 <- renderTable({
     tab1 <- assembled_graphics_data() %>%
       filter(!is.na(x_mut) & !is.na(y)) %>%
-      '['("x_mut") %>%
+      "["("x_mut") %>%
       table() %>%
       as.data.frame.table()
     names(tab1) <- c(paste0(input$var_x, ", AA change(s)"), "n")
     tab1
   })
-  
+
   output$fig1 <- renderPlot({
     if (input$show_mut) {
       gg <- assembled_graphics_data() %>%
@@ -101,109 +108,137 @@ function(input, output) {
     }
     if (input$mark_mut) {
       gg <- gg +
-        geom_point(aes(col = x_mutcat, alpha = x_mutcat, shape = x_mutcat), 
-          position = position_jitter(h = 0,  w = 0.1)) + 
-        geom_boxplot(col = "darkred", varwidth = TRUE,
-          fill = "transparent", outlier.colour = "transparent") +
-        scale_colour_manual(values = colmutcat, na.value = "black", guide = FALSE) + 
-        scale_alpha_manual(values = alphamutcat, na.value = 1, guide = FALSE) + 
-        scale_shape_manual(values = shapemutcat, na.value = 4, guide = FALSE) + 
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+        geom_point(
+          aes(col = x_mutcat, alpha = x_mutcat, shape = x_mutcat),
+          position = position_jitter(h = 0, w = 0.1)
+        ) +
+        geom_boxplot(
+          col = "darkred", varwidth = TRUE,
+          fill = "transparent", outlier.colour = "transparent"
+        ) +
+        scale_colour_manual(values = colmutcat, na.value = "black", guide = FALSE) +
+        scale_alpha_manual(values = alphamutcat, na.value = 1, guide = FALSE) +
+        scale_shape_manual(values = shapemutcat, na.value = 4, guide = FALSE) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
         labs(
-          x = paste0(input$var_x, ", predicted somatic non-silent mutation"), 
-          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"))
+          x = paste0(input$var_x, ", predicted somatic non-silent mutation"),
+          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)")
+        )
     } else {
       gg <- gg +
-        geom_point(shape = 1, alpha = 0.5, 
-          position = position_jitter(h = 0,  w = 0.1)) + 
-        geom_boxplot(col = "darkred", varwidth = TRUE,
-          fill = "transparent", outlier.colour = "transparent") +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+        geom_point(
+          shape = 1, alpha = 0.5,
+          position = position_jitter(h = 0, w = 0.1)
+        ) +
+        geom_boxplot(
+          col = "darkred", varwidth = TRUE,
+          fill = "transparent", outlier.colour = "transparent"
+        ) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
         labs(
-          x = paste0(input$var_x, ", predicted somatic non-silent mutation"), 
-          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"))
+          x = paste0(input$var_x, ", predicted somatic non-silent mutation"),
+          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)")
+        )
     }
-    if (input$by_subtype)
-      gg <- gg + facet_wrap(~ subtype2, nrow = 2, as.table = FALSE)
+    if (input$by_subtype) {
+      gg <- gg + facet_wrap(~subtype2, nrow = 2, as.table = FALSE)
+    }
     plot(gg)
   })
-  
+
   output$fig2 <- renderPlot({
     gg <- assembled_graphics_data() %>%
       filter(!is.na(x_gistic) & !is.na(y)) %>%
-      ggplot(aes(x = x_gistic, y = y)) 
+      ggplot(aes(x = x_gistic, y = y))
     if (input$mark_mut) {
       gg <- gg +
-        geom_point(aes(col = x_mutcat, alpha = x_mutcat, shape = x_mutcat), 
-          position = position_jitter(h = 0,  w = 0.1)) + 
-        geom_boxplot(col = "darkred", varwidth = TRUE,
-          fill = "transparent", outlier.colour = "transparent") +
-        scale_colour_manual(values = colmutcat, na.value = "black") + 
-        scale_alpha_manual(values = alphamutcat, na.value = 1) + 
-        scale_shape_manual(values = shapemutcat, na.value = 4) + 
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+        geom_point(aes(col = x_mutcat, alpha = x_mutcat, shape = x_mutcat),
+          position = position_jitter(h = 0, w = 0.1)
+        ) +
+        geom_boxplot(
+          col = "darkred", varwidth = TRUE,
+          fill = "transparent", outlier.colour = "transparent"
+        ) +
+        scale_colour_manual(values = colmutcat, na.value = "black") +
+        scale_alpha_manual(values = alphamutcat, na.value = 1) +
+        scale_shape_manual(values = shapemutcat, na.value = 4) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
         labs(
-          x = paste0(input$var_x, ", putative CNA (GISTIC)"), 
+          x = paste0(input$var_x, ", putative CNA (GISTIC)"),
           y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"),
-          col = input$var_x, alpha = input$var_x, shape = input$var_x)
+          col = input$var_x, alpha = input$var_x, shape = input$var_x
+        )
     } else {
       gg <- gg +
-        geom_point(shape = 1, alpha = 0.5, 
-          position = position_jitter(h = 0,  w = 0.1)) + 
-        geom_boxplot(col = "darkred", varwidth = TRUE,
-          fill = "transparent", outlier.colour = "transparent") +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+        geom_point(
+          shape = 1, alpha = 0.5,
+          position = position_jitter(h = 0, w = 0.1)
+        ) +
+        geom_boxplot(
+          col = "darkred", varwidth = TRUE,
+          fill = "transparent", outlier.colour = "transparent"
+        ) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
         labs(
-          x = paste0(input$var_x, ", putative CNA (GISTIC)"), 
-          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"))
+          x = paste0(input$var_x, ", putative CNA (GISTIC)"),
+          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)")
+        )
     }
-    if (input$by_subtype)
-      gg <- gg + facet_wrap(~ subtype2, nrow = 2, as.table = FALSE)
+    if (input$by_subtype) {
+      gg <- gg + facet_wrap(~subtype2, nrow = 2, as.table = FALSE)
+    }
     plot(gg)
   })
-  
+
   output$fig3 <- renderPlot({
     gg <- assembled_graphics_data() %>%
       filter(!is.na(x_rna) & !is.na(y)) %>%
-      ggplot(aes(x = x_rna, y = y)) 
+      ggplot(aes(x = x_rna, y = y))
     if (input$mark_mut) {
       gg <- gg +
-        geom_point(aes(col = x_mutcat, alpha = x_mutcat, shape = x_mutcat)) + 
-        scale_colour_manual(values = colmutcat, na.value = "black") + 
-        scale_alpha_manual(values = alphamutcat, na.value = 1) + 
-        scale_shape_manual(values = shapemutcat, na.value = 4) + 
+        geom_point(aes(col = x_mutcat, alpha = x_mutcat, shape = x_mutcat)) +
+        scale_colour_manual(values = colmutcat, na.value = "black") +
+        scale_alpha_manual(values = alphamutcat, na.value = 1) +
+        scale_shape_manual(values = shapemutcat, na.value = 4) +
         labs(
-          x = paste0(input$var_x, ", mRNA expression (log2 RNA-seq)"), 
-          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"), 
-          col = input$var_x, alpha = input$var_x, shape = input$var_x)
+          x = paste0(input$var_x, ", mRNA expression (log2 RNA-seq)"),
+          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"),
+          col = input$var_x, alpha = input$var_x, shape = input$var_x
+        )
     } else {
       gg <- gg +
-        geom_point(shape = 1, alpha = 0.5) + 
+        geom_point(shape = 1, alpha = 0.5) +
         labs(
-          x = paste0(input$var_x, ", mRNA expression (log2 RNA-seq)"), 
-          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)"))
+          x = paste0(input$var_x, ", mRNA expression (log2 RNA-seq)"),
+          y = paste0(input$var_y, ", mRNA expression (log2 RNA-seq)")
+        )
     }
-    if (input$fig3_smooth_method != "(none)")
+    if (input$fig3_smooth_method != "(none)") {
       gg <- gg + geom_smooth(col = "darkred", method = input$fig3_smooth_method)
-    if (input$by_subtype)
-      gg <- gg + facet_wrap(~ subtype2, nrow = 2, as.table = FALSE)
+    }
+    if (input$by_subtype) {
+      gg <- gg + facet_wrap(~subtype2, nrow = 2, as.table = FALSE)
+    }
     plot(gg)
   })
-  
+
   output$tab2 <- renderTable({
     graphics_data <- assembled_graphics_data()
     r_all <- cor(
-      graphics_data$x_rna, graphics_data$y, 
-      use = "complete.obs", method = "spearman")
+      graphics_data$x_rna, graphics_data$y,
+      use = "complete.obs", method = "spearman"
+    )
     r_subtype <- unlist(lapply(
-      split(graphics_data, graphics_data$subtype), 
-      function(df) cor(df$x_rna, df$y, 
-        use = "complete.obs", method = "spearman")))
+      split(graphics_data, graphics_data$subtype),
+      function(df) cor(df$x_rna, df$y,
+          use = "complete.obs", method = "spearman"
+        )
+    ))
     tab2 <- data.frame(
-      grp = c("(all)", names(r_subtype)), 
-      r = c(r_all, r_subtype))
+      grp = c("(all)", names(r_subtype)),
+      r = c(r_all, r_subtype)
+    )
     names(tab2) <- c("Molecular subtype", "r")
     tab2
   })
-  
 }
